7abc43c3982adf97011060a2f9328ccb92ace3ce
Dimitar Yurukov <mscalindt@protonmail.com>
2019-10-10 18:56:12 +0300

    ALSA: compress: Fix NEXT_TRACK_PARAM declaration
    Signed-off-by: Dimitar Yurukov <mscalindt@protonmail.com>

--- include/uapi/sound/compress_offload.h
+++ include/uapi/sound/compress_offload.h
@@ -195,7 +195,7 @@ struct snd_compr_metadata {
 #define SNDRV_COMPRESS_NEXT_TRACK	_IO('C', 0x35)
 #define SNDRV_COMPRESS_PARTIAL_DRAIN	_IO('C', 0x36)
 #define SNDRV_COMPRESS_SET_NEXT_TRACK_PARAM\
-					_IOW('C', 0x80, union snd_codec_options)
+					_IOW('C', 0x37, union snd_codec_options)
 /*
  * TODO
  * 1. add mmap support
